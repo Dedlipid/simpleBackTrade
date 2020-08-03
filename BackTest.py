b24e242b7f75b9ed377e6ce2f364e64456716d5f
Bamdad Shamaei <bamshamaei@gmail.com>
2020-08-03 17:00:43 -0400

    Update BackTest.py

--- BackTest.py
+++ BackTest.py
@@ -1,44 +1,46 @@
 import pandas as pd
-T = pd.read_csv("assets.csv",index_col="index")
-T["last_buy"] = T["last_price"] = 0#Ass_Name, Quan, Last_buy,last_price
+T = pd.read_csv("assets.csv")
+T["Quantity"] = T["last_buy"] = T["last_price"] = 0  # Ass_Name, Quantity, Last_buy,last_price
 C_0 = [15000]
 cash_total = C_0[0]
 cash_add = 0
 cash_out = 0
 V = cash_total/2
 for i in range(len(T["Assets"])):
+
     temp = pd.read_csv(T["Assets"][i] + ".csv", index_col="Date")
     his = len(temp["Open"])
-    if cash_total < C_0[0]/5 and cash_out >= max(C_0[0]/4,2500) and True:
-        cash_total += max(C_0[0]/4,2500)
-        cash_out -= max(C_0[0]/4,2500)
+
+    if cash_total < C_0[0]/5 and cash_out >= max(C_0[0]/4, 2500) and True:
+        cash_total += max(C_0[0]/4, 2500)
+        cash_out -= max(C_0[0]/4, 2500)
+
     if cash_total > C_0[0]*1.1 and True:
         cash_out = C_0[0]*0.1
         cash_total -= cash_out
-    for j in range(his-250, his-5):  ####RUNNING PERIOD
+
+    for j in range(his-250, his):  # RUNNING PERIOD
         # Date,Open,High,Low,Close,Adj Close,Volume,STA,LTA,HV,LV
-        q = round(V/temp["Close"][j])-1
-        T.iat[i,3] = temp["Close"][j]
-        if T.iat[i, 1] > 0: #if any of the asset is owned, we check whether to buy or sell
-            if temp["Close"][j] >= temp["STA"][j]:#sells if the price is higher then
-                cash_total += temp["Close"][j]*T.iat[i, 1]# the 5 day average
+        q = max(round(V/temp["Close"][j])-1, 0)
+        T.iat[i, 3] = temp["Close"][j]
+
+        if T.iat[i, 1] > 0:  # if any of the asset is owned, we check whether to buy or sell
+            if temp["Close"][j] >= temp["STA"][j]:  # sells if the price is higher then
+                cash_total += temp["Close"][j]*T.iat[i, 1]  # the 5 day average
                 T.iat[i, 1] = 0
-            if temp["Close"][j] <= T.iat[i, 2] and True:#if true the risky strategy
-                if cash_total >= temp["Close"][j] * q:  #will buy annadditional unnit
+            if temp["Close"][j] <= T.iat[i, 2] and True:  # if true the risky strategy
+                if cash_total >= temp["Close"][j] * q:  # will buy an additional unit
                     cash_total -= temp["Close"][j] * q
                     T.iat[i, 1] += q
 
-        if temp["HV"][j] and temp["LV"][j]:#if the high and low have fallen for two consecutive days
-            if temp["STA"][j] >= temp["Close"][j] >= temp["LTA"][j]:#if the price is higher then the
-                if cash_total >= temp["Close"][j]*q: #long term but bellow the short term average
+        if temp["HV"][j] and temp["LV"][j]:  # if the high and low have fallen for two consecutive days
+            if temp["STA"][j] >= temp["Close"][j] >= temp["LTA"][j]:  # if the price is higher then the
+                if cash_total >= temp["Close"][j]*q:  # long term but bellow the short term average
                     cash_total -= temp["Close"][j]*q
                     T.iat[i, 1] += q
                     T.iat[i, 2] = temp["Close"][j]
-T["V"] = T["Quant"]*T["last_price"]
+T["V"] = T["Quantity"]*T["last_price"]
 assets = T["V"].sum()
-
-
-
 
 if __name__ == '__main__':
     print(f'cash={cash_total + cash_out},'
