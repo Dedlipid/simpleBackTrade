1709da3b4fb9f193c059d62f58b21e88ae1f2087
Bamdad Shamaei <bamshamaei@gmail.com>
2020-08-03 16:59:35 -0400

    Update DataFetch.py

--- DataFetch.py
+++ DataFetch.py
@@ -5,36 +5,46 @@ Gets and updates data of tracked assets, which are kept in assets.csv
 As of now, assets in assets.csv have to be initialized or removed by hand
 """
 
-def nDA(n,i,data):
+
+def nda(n, i, data):
     s = 0.0
     for j in range(n):
         s += data[i-j-1][3]
     return s/n
-def HV(n, data):
+
+
+def hv(n, data):
     return data[n-2][1] > data[n-1][1] > data[n][1]
-def LV(n,data):
+
+
+def lv(n, data):
     return data[n-2][2] > data[n-1][2] > data[n][2]
-T = pd.read_csv("assets.csv", index_col="index")
-def I():
+
+
+T = pd.read_csv("assets.csv")
+
+
+def init():
     for i in range(len(T["Assets"])):
-        sta=[]
-        lta=[]
-        hv=[]
-        lv=[]
-        temp = yf.download(T.at[i,"Assets"],period="max")
+        sta = []
+        lta = []
+        h = []
+        l = []
+        temp = yf.download(T.at[i, "Assets"], period="max")
         for k in range(0, len(temp["Open"])):
-            sta.append(nDA(5, k, temp.values))
-            lta.append(nDA(200, k, temp.values))
-            hv.append(HV(k, temp.values))
-            lv.append(LV(k, temp.values))
+            sta.append(nda(5, k, temp.values))
+            lta.append(nda(200, k, temp.values))
+            h.append(hv(k, temp.values))
+            l.append(lv(k, temp.values))
         temp["STA"] = sta
         temp["LTA"] = lta
-        temp["HV"] = hv
-        temp["LV"] = lv
+        temp["HV"] = h
+        temp["LV"] = l
         # Date,Open,High,Low,Close,Adj Close,Volume
         temp.to_csv(T["Assets"][i] + ".csv")
         print(f'{i + 1}/20')
-    return ("DataFetch is done")
+    return "DataFetched"
+
 
 if __name__ == "__main__":
-    I()
+    print(init())
